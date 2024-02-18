8911ac3924fc83a420e0d678b4452a36389aaff2
lossi1997 <tuukka.orava@outlook.com>
2024-02-18 17:00:45 +0200

    added split-safe hidden dir creation when saving

--- src/filehandling.py
+++ src/filehandling.py
@@ -1,7 +1,10 @@
 
 import shutil
+import os
 
 from file_dataclass import File
+
+# FIXME error when split-safe folder does not exist
 
 
 class Filehandler:
@@ -12,4 +15,7 @@ class Filehandler:
 
     def backup(self):
         for file in self.fileobjs:
+            if not os.path.exists(file.dest_path):
+                os.mkdir(file.dest_path)
+                os.system("attrib +h " + file.dest_path)
             shutil.copy(file.orig_path, file.dest_path)
